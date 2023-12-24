caa83c78fad535d537672af7d21874f0e515c832
aozp73 <aozp73@naver.com>
2023-12-24 16:06:13 +0900

    코드 일부 수정

--- src/test/java/com/example/book/web/BookControllerIntegrationTest.java
+++ src/test/java/com/example/book/web/BookControllerIntegrationTest.java
@@ -1,12 +1,22 @@
 package com.example.book.web;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
 
 /** 
  * Controller 단위 테스트 
  * - 필요: Controller / Filter / ControllerAdvice 등
  */
-@WebMvcTest
+@Transactional // Test에서는 각 테스트 종료 후, 자동 rollback
+@AutoConfigureMockMvc // MockMvc IoC 등록
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 public class BookControllerIntegrationTest {
-    
+
+    @Autowired
+    private MockMvc mockMvc;
 }
